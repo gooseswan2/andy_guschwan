c8a2800db5040a7acc20dfaef17aaecafb5015a5
gooseswan2 <aguschwan@gmail.com>
2018-12-08 17:48:51 -0600

    Add files via upload

--- server.py
+++ server.py
@@ -2,17 +2,28 @@ from flask import Flask, render_template, request, redirect, session
 import random
 app = Flask(__name__)
 app.secret_key = "afjasfkja87"
-@app.route('/')
+@app.route('/', methods=["post"])
 def index():
     num = random.randrange(0,101)
     session['num'] = num
     print(session['num'])
     return render_template("index.html")
 
-@app.route('/guess', methods=["POST"])
-def guess(request):
-    if int(session['num']) == int(request.form['guess']):
-        result = True
+@app.route('/guess', methods=["post"])
+def guess():
+    num = session['num']
+    guess = request.form['guess']
+    result = ""
+    if guess != "":
+        if int(guess) > int(num):
+            result = "Too high"
+        elif int(guess) < int(num):
+            result = "Too low"
+        elif int(guess) == int(num):
+            result = "You got it"
+        else:
+            result = ""
+    
     return render_template("index.html", result=result)
 
 if __name__=="__main__":
